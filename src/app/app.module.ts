2f15ccf6510c252edc63c059b3665d62586c6a3f
osiykm <osiykmukola@gmail.com>
2017-09-26 23:37:19 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import {ROUTING} from './app.routing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HomeComponent} from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    CategoriesComponent,
+    AuthorsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
+    HttpModule,
     ClarityModule.forRoot(),
     ROUTING
   ],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,17 @@
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AuthorsComponent} from './authors/authors.component';
+import {CategoriesComponent} from './categories/categories.component';
+import {BooksComponent} from './books/books.component';
 
 
 export const ROUTES: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'authors', component: AuthorsComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'books', component: BooksComponent}
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES);
